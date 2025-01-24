1b9494fa6b1cdc1b8a00c9093fadf02bdc2ffe66
HaguraK <hendrewlost@gmail.com>
2025-01-23 21:01:22 -0300

    primeiro comit

---
+++ comentarios.py
@@ -0,0 +1,10 @@
+#  API - e um lugar para disponibilizar recursos e/ou funcionalidades
+#  1. Objetivo - Criar um api que disponibiliza a consulta, criacao, edicao e exclusao de livros
+#  2. URL Base - localhost
+#  3. EndPoints - 
+#       - localhost/livros (GET)
+#       - localhost/livros (POST)
+#       - localhost/livros/id (GET)
+#       - localhost/livros/id (PUT)
+#       - localhost/livros/id (DELETE)
+#   4. Quais Recursos - Livros
